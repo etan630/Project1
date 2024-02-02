7378275a790f670ab70f26f6bb7ba87cdc9396b5
margaretsong <msong321@gatech.edu>
2024-02-02 02:30:30 -0500

    list updates

--- app/src/main/java/com/example/project1/ListFragment.java
+++ app/src/main/java/com/example/project1/ListFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.project1.data.list.Todo;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,13 +26,14 @@ public class ListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "output";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     List<ToDoInput> todoList = new ArrayList<ToDoInput>();
-    List<AssignmentInput> assignmentInput = new ArrayList<AssignmentInput>();
-    List<ExamInput> examInput = new ArrayList<ExamInput>();
+    List<AssignmentInput> assignmentList = new ArrayList<AssignmentInput>();
+    List<ExamInput> examList = new ArrayList<ExamInput>();
 
     public ListFragment() {
         // Required empty public constructor
@@ -60,8 +65,11 @@ public class ListFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         fillTodoList();
+        Log.d(TAG, "onCreate: " + todoList.toString());
         fillAssignmentList();
+        Log.d(TAG, "onCreate: " + assignmentList.toString());
         fillExamList();
+        Log.d(TAG, "onCreate: " + examList.toString());
     }
 
     private void fillTodoList() {
@@ -70,18 +78,26 @@ public class ListFragment extends Fragment {
         ToDoInput td3 = new ToDoInput("do laundry");
         ToDoInput td4 = new ToDoInput("clean room");
         ToDoInput td5 = new ToDoInput("get groceries");
-    }
 
+        todoList.addAll(Arrays.asList(td1, td2, td3, td4, td5));
+
+    }
     private void fillAssignmentList() {
         AssignmentInput a1 = new AssignmentInput("CS2340", "Project 1", 2.6);
         AssignmentInput a2 = new AssignmentInput("KOR1002", "Unit 8 Workbook", 2.8);
         AssignmentInput a3 = new AssignmentInput("CS2050", "Zybooks 4", 2.3);
         AssignmentInput a4 = new AssignmentInput("EAS1600", "Lab Assignment", 2.12);
+
+        assignmentList.addAll(Arrays.asList(a1, a2, a3, a4));
+
     }
 
     private void fillExamList() {
         ExamInput e1 = new ExamInput("CS2050 midterm 1", 2.8, 9.30, "CULC144");
         ExamInput e2 = new ExamInput("KOR1002 midterm 1", 2.10, 12.30, "online");
+
+        examList.addAll(Arrays.asList(e1, e2));
+
     }
 
     @Override
