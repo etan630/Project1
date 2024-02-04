9566c59e4cda7dfcc1a5613dddb662c31b6c678a
etan630 <etan61@gatech.edu>
2024-02-03 19:43:21 -0500

    Delete Course button along with confirmaton page

--- app/src/main/java/com/example/project1/data/CourseListAdapter.java
+++ app/src/main/java/com/example/project1/data/CourseListAdapter.java
@@ -1,26 +1,34 @@
 package com.example.project1.data;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.project1.R;
+import com.example.project1.viewmodel.AppViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CourseListAdapter extends ArrayAdapter<Course> {
     private List<Course> courses;
+    private Context context;
+
 
     public CourseListAdapter(Context context, ArrayList<Course> courses) {
         super(context, 0, courses);
         this.courses = courses;
+        this.context = context;
     }
 
     // Override the getView method to customize the appearance of each item in the ListView
@@ -44,6 +52,10 @@ public class CourseListAdapter extends ArrayAdapter<Course> {
 //        instructorNameTextView.setText(course.getInstructor());
 //        timeTextView.setText(course.getTime());
 
+        //click listeners for delete
+        Button deleteButton = convertView.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(v -> showDeleteConfirmation(course));
+
         // Return the updated convertView
         return convertView;
     }
@@ -52,5 +64,20 @@ public class CourseListAdapter extends ArrayAdapter<Course> {
     public void setCourses(List<Course> courses) {
         this.courses = courses;
         notifyDataSetChanged();
+    }
+
+    private void showDeleteConfirmation(Course course) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Delete Course");
+        builder.setMessage("Are you sure you want to delete this course " + course.getName() + "?");
+        builder.setPositiveButton("Yes", (dialog, which) -> deleteCourse(course));
+        builder.setNegativeButton("No", null);
+        builder.show();
+    }
+
+    private void deleteCourse(Course course) {
+        courses.remove(course);
+        AppViewModel viewModel = new ViewModelProvider((FragmentActivity) context).get(AppViewModel.class);
+        viewModel.removeCourse(course);
     }
 }
