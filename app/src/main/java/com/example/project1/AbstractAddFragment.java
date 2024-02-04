7cc087bb9ff5a355d83083a302e6f7d9f9f4adc1
Ethan Liang <e3lcool@gmail.com>
2024-02-04 17:24:27 -0500

    working spinners for course dropdowns on add menus

--- app/src/main/java/com/example/project1/AbstractAddFragment.java
+++ app/src/main/java/com/example/project1/AbstractAddFragment.java
@@ -7,14 +7,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
+import com.example.project1.viewmodel.AppViewModel;
 
 /**
  * A screen with a back button, an "add" button, and a placeholder for custom
  * implementation-specific form content.
  */
 public abstract class AbstractAddFragment extends Fragment {
+    protected AppViewModel viewModel;
+
     /**
      * Gets the id of the View (typically a form with text inputs) to be inserted into this add
      * screen. Should be overriden.
@@ -26,9 +31,19 @@ public abstract class AbstractAddFragment extends Fragment {
     }
 
     /**
-     * Logic to be run when the add button on the add screen is clicked.
+     * Logic to be run after the form content is inflated from XML, e.g. to activate elements.
      *
      * @param thisFragment this fragment's view
+     */
+    protected void afterInflating(View thisFragment) {
+    }
+
+    ;
+
+    /**
+     * Logic to be run when the add button on the add screen is clicked.
+     *
+     * @param thisFragment  this fragment's view
      * @param navController the nav controller to use to navigate away from this screen
      * @return OnCLickListener
      */
@@ -37,6 +52,8 @@ public abstract class AbstractAddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        viewModel = new ViewModelProvider(getActivity()).get(AppViewModel.class);
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_dialogue, container, false);
 
@@ -46,6 +63,7 @@ public abstract class AbstractAddFragment extends Fragment {
         if (getFormContent() != 0) {
             inflater.inflate(getFormContent(), view.findViewById(R.id.form_placeholder));
         }
+        afterInflating(view);
 
         return view;
     }
